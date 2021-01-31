a341ffe037594bc82384deb15d6c05da3b1ef1d2
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-01-31 21:01:50 +0530

    Added migrations for tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_01_31_150127) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "name"
-    t.text "desc"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.text "title"
     t.datetime "created_at", precision: 6, null: false
