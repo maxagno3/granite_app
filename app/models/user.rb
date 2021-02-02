88be051ce8bf8b8a09a90db62a26026a4d35bfff
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-02-03 01:18:38 +0530

    Added comments to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
